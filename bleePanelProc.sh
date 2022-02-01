22e59feff67756a3f4556d86c566caf8d2e10905
Mohsen Banan <form@mohsen.banan.1.byname.net>
2022-02-01 01:55:32 +0000

    dblocked.

--- bleePanelProc.sh
+++ bleePanelProc.sh
@@ -13,13 +13,13 @@ __copying__="
 
 ####+BEGIN: bx:bsip:bash:seed-spec :types "seedBleePanelProc.sh"
 SEED="
-*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedBleePanelProc.sh]] | 
+*  /[dblock]/ /Seed/ :: [[file:/bisos/core/bsip/bin/seedBleePanelProc.sh]] |
 "
 FILE="
-*  /This File/ :: /bisos/panels/non-libre-halaal/bleePanelProc.sh 
+*  /This File/ :: /bisos/git/auth/bxRepos/blee-binders/non-libre-halaal/bleePanelProc.sh
 "
 if [ "${loadFiles}" == "" ] ; then
-    /bisos/core/bsip/bin/seedBleePanelProc.sh -l $0 "$@" 
+    /bisos/core/bsip/bin/seedBleePanelProc.sh -l $0 "$@"
     exit $?
 fi
 ####+END:
